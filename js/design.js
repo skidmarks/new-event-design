e56729c778687b81190c01f28bdb1b0a1420fc9d
skidmarks <florakid@naver.com>
2023-03-20 14:44:27 +0900

    completed layer, joined layer added

--- js/design.js
+++ js/design.js
@@ -21,13 +21,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const phoneTitle = document.querySelector('.tit_phone');
     const infoTitle = document.querySelector('.info_tit');
     const btnCloseInfo = document.querySelector('.btn_close_info_cert');
-    const layerCompleted = document.querySelector('.completed_wrap');
+    const layerCompleted = document.querySelector('.completed_wrap'); // 참여 성공 레이어
+    const layerJoinedYet = document.querySelector('.joined_wrap'); // 이미 참여 레이어
+    const certNumTester = document.querySelector('.cert_num'); // 인증번호 입력 레이어
+    const btnCloseCompleted = document.querySelector('.btn_close_completed'); // 참여성공 레이어 닫기 버튼
+    const btnCloseJoined = document.querySelector('.btn_close_joined'); // 이미참여 레이어 닫기 버튼
 
-    const certNumTester = document.querySelector('.cert_num');
-
-    console.log(inputName.value);
-
-
+    // 랜딩페이지 상담하기 버튼
     btnJoinEvent.addEventListener('click', function(e) {
         e.preventDefault();
         if (!layerEventForm.classList.contains('shown')) {
@@ -35,6 +35,8 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    // 상담하기 레이어 전체 닫기
+    // 닫기 버튼 시 레이어 초기화(reset)
     btnCloseFormLayer.addEventListener('click', function(e) {
         e.preventDefault();
         // console.log(this.parentElement);
@@ -49,11 +51,25 @@ document.addEventListener('DOMContentLoaded', function() {
         phoneTitle.style.display = "block";
     });
 
+    // input name Element blur, focus 이벤트 설정
+    // 선 색상 변경 blur 회색, focus 블루
+    inputName.addEventListener('blur', function(e) {
+        console.log(this.parentElement);
+        this.parentElement.classList.add('blur');
+    });    
+    inputName.addEventListener('focus', function(e) {
+        console.log(this.parentElement);
+        this.parentElement.classList.remove('blur');
+    });    
+
+    // 이름 확인 버튼 보이기
+    // 상담 대상자 이름 입력값 넣기
     inputName.addEventListener('input', function(e) {
         btnConfName.style.display = "block";
         personName.textContent = e.target.value;
     });
 
+    // 휴대폰번호 입력, 입력값의 길이가 11줄일때 입력자 이름 보이기
     inputPhone.addEventListener('input', function(e) {
         btnConfPhone.style.display = "block";
         if(e.target.value.length == 11) {
@@ -65,6 +81,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    // 이름 확인 버튼
     btnConfName.addEventListener('click', function(e) {
         e.preventDefault();
         stepPhone.style.display = "block";
@@ -74,6 +91,8 @@ document.addEventListener('DOMContentLoaded', function() {
         inputName.classList.add('blur');
     });
     
+    // 인증하기 버튼
+    // 퍼미션 레이어 열림
     btnConfPhone.addEventListener('click', function(e) {
         e.preventDefault();
         pmask.style.display = "block";
@@ -81,6 +100,8 @@ document.addEventListener('DOMContentLoaded', function() {
         bodyEle.classList.add('over_hide');
     });
 
+
+    // 퍼미션 레이어 닫기 버튼
     btnClosePm.addEventListener('click', function(e) {
         e.preventDefault();
         pmask.style.display = "none";
@@ -89,6 +110,7 @@ document.addEventListener('DOMContentLoaded', function() {
         resetInputCheck();
     });
 
+    // 퍼미션 레이어 동의하기 버튼
     btnAgreePm.addEventListener('click', function(e) {
         e.preventDefault();
         layerCertNumb.classList.add('shown');
@@ -97,17 +119,21 @@ document.addEventListener('DOMContentLoaded', function() {
         bodyEle.classList.remove('over_hide');
     });
 
+    // 인증번호 닫기(뒤로가기 버튼)
     btnCloseInfo.addEventListener('click', function(e) {
         e.preventDefault();
         layerCertNumb.classList.remove('shown');
     });
 
+    // input value 리셋
     function resetInputText() {
         const allFormInputs = layerEventForm.querySelectorAll('input');
         allFormInputs.forEach(function(input) {
             input.value = '';
         });
     }
+
+    // input checked checked 해제하기
     function resetInputCheck() {
         const allPmissionInputs = pmLayer.querySelectorAll('input');
         allPmissionInputs.forEach(function(check) {
@@ -115,8 +141,16 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    //인증번호 테스트
+    //인증번호 테스트, 
     certNumTester.addEventListener('change', function(e) {
+        if(e.target.value.length != 6){
+            alert('잘못된 번호입니다.');
+        }
+
+        if (inputPhone.value == "01012345678") {
+            layerJoinedYet.style.display = "flex";
+        }
+
         if(e.target.value == "123456") {
             layerCompleted.style.display = "flex"
         } else{
@@ -128,64 +162,65 @@ document.addEventListener('DOMContentLoaded', function() {
 
 ;(function ($) {
 
-$(function(){
+    // 퍼미션 동의 버튼
+    $(function(){
 
-var $btnToggle = $('.btn_details');
-var $allAgrBox = $('.prm_wrap');
+        var $btnToggle = $('.btn_details');
+        var $allAgrBox = $('.prm_wrap');
+        var $chkAllAgr01 = $('.chk_all');
+        var $check = $('.chk');	
+        var $checkOpt = $('.chk.opt');	
+        var $checkReq = $('.chk.req');
+        var $required = $('.required');
 
-var $chkAllAgr01 = $('.chk_all');
-var $check = $('.chk');	
-var $checkOpt = $('.chk.opt');	
-var $checkReq = $('.chk.req');
-var $required = $('.required');
-
-$allAgrBox.hide();
-
-
-// 동의 상세 열고 닫는 토글
-var agreeToggle = function(e) {
-    var $that = $(this);
-    var $eachBox = $that.parent().find("+.prm_wrap");
-    var $eachToggle = $that.siblings(".btn_details");
-
-    $eachToggle.show();
+        $allAgrBox.hide();
 
 
-    // this가 checkbox인지 아닌지
-    if (!$that.is(':checkbox')) {
-        e.preventDefault();		
+        // 동의 상세 열고 닫는 토글
+        var agreeToggle = function(e) {
+            var $that = $(this);
+            var $eachBox = $that.parent().find("+.prm_wrap");
+            var $eachToggle = $that.siblings(".btn_details");
 
-        if ($eachBox.is(':hidden')) {
-            $eachBox.removeClass("fix_h").show();
-            $that.text('닫기');
-        }else {
-            $eachBox.hide();
-            $that.text('보기');
-        }
-    }else {
-        if ($eachBox.is(':hidden')) {
-            $eachBox.removeClass("fix_h").show();
-            $that.text('닫기');
-        }else {
-            $eachBox.hide();
-            $that.text('보기');
-        }				
-    }	
-};
+            $eachToggle.show();
 
-// 버튼 클릭 시
-$btnToggle.each(function () {		
-    var $this = $(this);
-    $this.on("click", agreeToggle);
-});
 
-// 전체동의
-$chkAllAgr01.change(function () {
-    $required.show().addClass("fix_h");
-    $checkOpt.prop('checked', $(this).prop("checked"));
-    $checkReq.prop('checked', $(this).prop("checked"));
-//			$(".btn_opt").trigger("click");
-});
-});
+            // this가 checkbox인지 아닌지
+            if (!$that.is(':checkbox')) {
+                e.preventDefault();		
+
+                if ($eachBox.is(':hidden')) {
+                    $eachBox.removeClass("fix_h").show();
+                    $that.text('닫기');
+                }else {
+                    $eachBox.hide();
+                    $that.text('보기');
+                }
+            }else {
+                if ($eachBox.is(':hidden')) {
+                    $eachBox.removeClass("fix_h").show();
+                    $that.text('닫기');
+                }else {
+                    $eachBox.hide();
+                    $that.text('보기');
+                }				
+            }	
+        };
+
+        // 버튼 클릭 시
+        $btnToggle.each(function () {		
+            var $this = $(this);
+            $this.on("click", agreeToggle);
+        });
+
+        // 전체동의
+        $chkAllAgr01.change(function () {
+            $required.show().addClass("fix_h");
+            $checkOpt.prop('checked', $(this).prop("checked"));
+            $checkReq.prop('checked', $(this).prop("checked"));
+        //			$(".btn_opt").trigger("click");
+        });
+
+    });
 
 })(jQuery);        
